49d6549143f2781c94f0389a11a0fce57dd020a4
ziyuwang <wangziyu@seas.upenn.edu>
2023-04-30 21:30:36 -0400

    glist

--- lolsfyi/src/pages/api/getPlayerList.ts
+++ lolsfyi/src/pages/api/getPlayerList.ts
@@ -6,12 +6,48 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     
     const { league, page=1, name, team, order , asc} = req.query;
     const conditions = {
-        league: league && `league='${league}'`,
-        team: team && `team=${team}`,
-        name: name && `name='${name}'`,
+        league: league && `Ps.league='${league}'`,
+        team: team && `Ps.Teamname=${team}`,
+        name: name && `Ps.name='${name}'`,
     };
     const whereClause = Object.values(conditions).filter(Boolean).join(' AND ');
-    const winrateq = `select playername,
+    // const winrateq = `select playername,
+    //             CASE
+    //               WHEN COUNT(*) < 6 THEN NULL
+    //               ELSE CAST(SUM(result) AS FLOAT) / COUNT(*)
+    //             END AS win_rate,
+    //             sum(result) as Numberofwins,
+    //             count(*)-sum(result) as NumberofLoses
+    //            from
+    //            (select playername,
+    //             CASE WHEN pp.Side = M.Win_side THEN 1 ELSE 0 END as result
+    //            from PlayerPerformance pp
+    //            join Matches M
+    //            on M.MatchID = pp.MatchID
+    //            join Players Ps
+    //            on Ps.Name = pp.playername
+    //            where Ps.League = M.League) resulttable
+    //            Group by  playername`
+         const pageSize = 20; // Define the number of results per page
+        const pageNumber = parseInt(page as string);
+        const offset = (pageNumber - 1) * pageSize;
+        if (!order){
+            try {
+                // const sqlQuery = `SELECT Teamname as teamname, 
+                // playername as id,
+                // League as league, 
+                // Position as position, 
+                // wr.win_rate as winrate, 
+                // wr.Numberofwins as numberofwin, 
+                // wr.NumberofLoses as numberofloses 
+                // FROM Players join (${winrateq}) as wr on wr.playername = name 
+                // ${whereClause ? `WHERE ${whereClause}` : ''}`; 
+
+                const sqlQuery =`
+                select playername,Teamname as teamname,
+                playername as id,
+                League as league,
+                Position as position,
                 CASE
                   WHEN COUNT(*) < 6 THEN NULL
                   ELSE CAST(SUM(result) AS FLOAT) / COUNT(*)
@@ -19,59 +55,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                 sum(result) as Numberofwins,
                 count(*)-sum(result) as NumberofLoses
                from
-               (select playername,
+               (select playername, Ps.Teamname, Ps.League, Ps.Position,
                 CASE WHEN pp.Side = M.Win_side THEN 1 ELSE 0 END as result
                from PlayerPerformance pp
                join Matches M
                on M.MatchID = pp.MatchID
                join Players Ps
                on Ps.Name = pp.playername
-               where Ps.League = M.League) resulttable
-               Group by  playername`
-    
-    // if (!page) {
-    //     if (!order){
-    //         try {
-    //             const sqlQuery = `SELECT * FROM Players join (${winrateq}) as wr on wr.playername = name ${whereClause ? `WHERE ${whereClause}` : ''}`;
-    //             const results = await db.query(sqlQuery);
-    //             // Close the database connection
-    //             await db.end();
-    //             // Send the fetched data as a response
-    //             res.status(200).json(results);
-    //           } catch (error) {
-    //             console.error(error);
-    //             res.status(500).json({ error: 'Error fetching data from the database' });
-    //           }
-    //     }
-    //     if (order) {
-    //         try {
-    //             const sqlQuery = `SELECT * FROM (SELECT * FROM Players join (${winrateq}) as wr on wr.playername = name ${whereClause ? `WHERE ${whereClause}` : ''}) as ps Order by ps.${order} ${(asc == '1') ? ` asc` : ' desc'}`;
-    //             const results = await db.query(sqlQuery);
-    //             // Close the database connection
-    //             await db.end();
-    //             // Send the fetched data as a response
-    //             res.status(200).json(results);
-    //           } catch (error) {
-    //             console.error(error);
-    //             res.status(500).json({ error: 'Error fetching data from the database' });
-    //           }
-    //     }
-    //   }
-    //   else{
-        const pageSize = 20; // Define the number of results per page
-        const pageNumber = parseInt(page as string);
-        const offset = (pageNumber - 1) * pageSize;
-        if (!order){
-            try {
-                const sqlQuery = `SELECT Teamname as teamname, 
-                playername as id,
-                League as league, 
-                Position as position, 
-                wr.win_rate as winrate, 
-                wr.Numberofwins as numberofwin, 
-                wr.NumberofLoses as numberofloses 
-                FROM Players join (${winrateq}) as wr on wr.playername = name 
-                ${whereClause ? `WHERE ${whereClause}` : ''}`; 
+               where Ps.League = M.League ${whereClause ? `and ${whereClause}` : ''}) resulttable
+               Group by  playername;`
+                
                 // LIMIT ${pageSize} OFFSET ${offset}`;
                 const results = await db.query(sqlQuery);
                 // Close the database connection
@@ -84,27 +77,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
               }
         }
 
-        if (order) {
-            try {
-                const sqlQuery = `SELECT * FROM (SELECT Teamname as teamname,  
-                  playername as id,League as league, Position as position, 
-                  wr.win_rate as winrate, 
-                  wr.Numberofwins as numberofwin, 
-                  wr.NumberofLoses as numberofloses 
-                  FROM Players join (${winrateq}) as wr on wr.playername = name 
-                  ${whereClause ? `WHERE ${whereClause}` : ''}) as ps 
-                  Order by ps.${order} ${(asc == '1') ? ` asc` : ' desc'}`;
-                  // LIMIT ${pageSize} OFFSET ${offset}`;
-                const results = await db.query(sqlQuery);
-                // Close the database connection
-                await db.end();
-                // Send the fetched data as a response
-                res.status(200).json(results);
-              } catch (error) {
-                console.error(error);
-                res.status(500).json({ error: 'Error fetching data from the database' });
-              }
-        }
+        // if (order) {
+        //     try {
+        //         const sqlQuery = `SELECT * FROM (SELECT Teamname as teamname,  
+        //           playername as id,League as league, Position as position, 
+        //           wr.win_rate as winrate, 
+        //           wr.Numberofwins as numberofwin, 
+        //           wr.NumberofLoses as numberofloses 
+        //           FROM Players join (${winrateq}) as wr on wr.playername = name 
+        //           ${whereClause ? `WHERE ${whereClause}` : ''}) as ps 
+        //           Order by ps.${order} ${(asc == '1') ? ` asc` : ' desc'}`;
+        //           // LIMIT ${pageSize} OFFSET ${offset}`;
+        //         const results = await db.query(sqlQuery);
+        //         // Close the database connection
+        //         await db.end();
+        //         // Send the fetched data as a response
+        //         res.status(200).json(results);
+        //       } catch (error) {
+        //         console.error(error);
+        //         res.status(500).json({ error: 'Error fetching data from the database' });
+        //       }
+        // }
   } catch (error) {
     res.status(500).json({ error: 'Error fetching data from database' });
   }
